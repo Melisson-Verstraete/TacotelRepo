655f6381fd6ab7860f5fe91a0280ae34994d828b
Melisson Verstraete <melisson.verstraete@gmail.com>
2021-07-29 15:10:59 +0200

    essaie de push pour montrer

--- app/src/main/java/com/helha/tacotel/AdminListArticlesActivity.java
+++ app/src/main/java/com/helha/tacotel/AdminListArticlesActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdminListArticlesActivity extends AppCompatActivity {//coucou
+public class AdminListArticlesActivity extends AppCompatActivity {//coucou Linda
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
