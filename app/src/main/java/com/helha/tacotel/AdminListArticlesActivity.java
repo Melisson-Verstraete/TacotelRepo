b379d13f39355567367a1b370202e1e07bb71f44
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-08-02 17:16:46 +0200

    Ajout d'un bouton permettant la création d'un article

--- app/src/main/java/com/helha/tacotel/AdminListArticlesActivity.java
+++ app/src/main/java/com/helha/tacotel/AdminListArticlesActivity.java
@@ -1,8 +1,11 @@
 package com.helha.tacotel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -14,6 +17,12 @@ import model.Article;
 import repository.ArticleRepository;
 
 public class AdminListArticlesActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_FORM_ADMIN_ARTICLE = 1;
+    public void addArticle(View view){
+        Intent intent = new Intent(AdminListArticlesActivity.this, FormAdminArticleActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_FORM_ADMIN_ARTICLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
